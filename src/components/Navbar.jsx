b7644f11c1f4482dad10d8115e8f09ffc2652d86
Ritesh Aditya <ritesh.aditya@invincix.com>
2025-02-07 18:17:06 +0530

    home screen changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,9 @@ import "./../styles/Navbar.css";
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="logo">My Portfolio</div>
-      <ul className="nav-links">
+    <nav className="navbar text-white py-4 px-6 flex justify-between items-center rounded-full m-8">
+      <div className="logo text-2xl font-bold">My Portfolio</div>
+      <ul className="nav-links flex space-x-70">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/projects">Projects</Link></li>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,21 @@ import "./../styles/Home.css";
 
 const Home = () => {
   return (
-    <div className="home-container">
-      <h1>Welcome to My Portfolio</h1>
-      <p>This is the home page where I will introduce myself.</p>
+    <div className="container mx-auto p-4 text-center">
+      <div className="inline-block border-2 border-black rounded-full px-6 py-3">Hello!</div>
+      <div className="intro text-8xl p-4 font-semibold">I'm <span className="text-orange-500">Ritesh</span>, <br />Software Developer</div>
+      <div className="flex justify-between mt-8 px-16">
+        <div className="max-w-sm p-6 bg-gray-50 rounded-lg">
+          <p className="text-lg">
+            Passionate about creating elegant solutions to complex problems. 
+          </p>
+        </div>
+        <div className="max-w-sm p-6 bg-gray-50 rounded-lg">
+          <p className="text-lg">
+            <span className="text-orange-500 font-bold text-2xl">1+</span> Years of Experience
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
